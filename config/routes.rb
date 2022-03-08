433787c538ee343300fc7a86fafed8b4f032b1a9
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-03-09 07:30:57 +0900

    トップページ作成1

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'static_pages#top'
+  get '/signup', to: 'users#new'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
