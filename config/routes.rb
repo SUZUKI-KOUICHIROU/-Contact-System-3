66536a3630a1e7b6e242aa3828e1c624fca1c7a5
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-07-16 19:23:01 +0900

    修正39

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  get  'users#show'
+
   get 'new_guardian', to: 'users#new_guardian'
   post 'create_guardian', to: 'users#create_guardian'
   
