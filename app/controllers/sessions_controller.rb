4c34368a78651e847cc10b96e1767350c3e1314d
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-07-18 07:55:26 +0900

    修正60

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,29 @@
 class SessionsController < ApplicationController
 
+  uri = URI.parse("https://api.line.me/oauth2/v2.1/token")
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = "application/x-www-form-urlencoded"
+    request.set_form_data(
+      "client_id" => "1657280915",
+      #"client_secret" => "xxx",
+      "code" => params[:code],
+      "grant_type" => "authorization_code",
+      "redirect_uri" => "https://arcane-peak-28945.herokuapp.com",
+    )
+
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+
+  JWT.decode(JSON.parse(response.body)["id_token"],"#{client_secret}")
+  
+  
+  
+  
   def new
   end
 
