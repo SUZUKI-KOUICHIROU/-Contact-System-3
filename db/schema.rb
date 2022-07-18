7a48fefdc4f3392db92e47aa2480fc5fced6fa14
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-07-19 07:31:29 +0900

    修正70

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_18_040143) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_18_221147) do
   create_table "admins", force: :cascade do |t|
     t.integer "line_messaging_id", null: false
     t.string "line_messaging_secret", null: false
