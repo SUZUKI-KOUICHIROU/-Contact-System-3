c2859acf16f362e15e75b81366047ec1d9bc6cfe
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-07-19 08:11:54 +0900

    修正74

--- app/models/line/api/oauth.rb
+++ app/models/line/api/oauth.rb
@@ -3,10 +3,10 @@ AUTH_URI = 'https://access.line.me/oauth2/v2.1/authorize'
 def auth_uri(state)
   params = {
     response_type: 'code',
-    client_id: @admin.line_login_id,
-    redirect_uri: callback_uri,
+    client_id: "1657280915",
+    redirect_uri: "https://arcane-peak-28945.herokuapp.com",
     state: state,
-    scope: 'openid',
+    scope: 'openid%20email',
     prompt: 'consent', # 必ずLINE認証を許可するようにするオプション
     bot_prompt: 'aggressive' # ログイン後に連携した公式アカウントと友だちになるか聞く画面を出してくれる
   }
