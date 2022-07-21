a86631099e6621c69b9ba660f3933a991801b68f
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-07-21 11:08:21 +0900

    修正96

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,13 @@
 class SessionsController < ApplicationController
 
+  def index
+    admin = Admin.find(params[:admin_id])
+
+    state = SecureRandom.hex(32)
+    session[:state] = state
+    redirect_to Line::Api::Oauth.new(admin).auth_uri(state)
+  end
+  
   def new
   end
 
