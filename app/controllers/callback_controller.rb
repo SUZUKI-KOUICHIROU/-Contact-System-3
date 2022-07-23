dbe492bbbdacd72f8855dc32b7ae6b497d435e3c
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-07-24 07:42:45 +0900

    修正15

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -3,9 +3,7 @@ class CallbackController < ApplicationController
     admin = Admin.find(params[:admin_id])
 
     # stateが異なっていたら例外を出す
-    unless params[:state] == session[:state]
-      flash[:danger] = '失敗しました。'
-    end
+    raise Line::InvalidState unless params[:state] == session[:state]
 
     line_user_id = Line::Api::Oauth.new(admin).line_user_id(params[:code])
     User.create!(line_user_id: line_user_id)
