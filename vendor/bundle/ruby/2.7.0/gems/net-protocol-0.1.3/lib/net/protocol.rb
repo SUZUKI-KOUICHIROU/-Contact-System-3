e820d7d60b0805d2ce1b43c672ef298e32052f9d
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-08-01 18:57:29 +0900

    修正5

--- vendor/bundle/ruby/2.7.0/gems/net-protocol-0.1.3/lib/net/protocol.rb
+++ vendor/bundle/ruby/2.7.0/gems/net-protocol-0.1.3/lib/net/protocol.rb
@@ -63,9 +63,9 @@ module Net # :nodoc:
   class ProtoUnknownError      < ProtocolError; end
   class ProtoServerError       < ProtocolError; end
   class ProtoAuthError         < ProtocolError; end
-  class ProtoCommandError      < ProtocolError; end
+  #class ProtoCommandError      < ProtocolError; end
   class ProtoRetriableError    < ProtocolError; end
-  ProtocRetryError = ProtoRetriableError
+  #ProtocRetryError = ProtoRetriableError
 
   ##
   # OpenTimeout, a subclass of Timeout::Error, is raised if a connection cannot
@@ -500,8 +500,8 @@ module Net # :nodoc:
   end
 
 
-  module NetPrivate   :nodoc: obsolete
-    Socket = ::Net::InternetMessageIO
+  module NetPrivate   #:nodoc: obsolete
+    #Socket = ::Net::InternetMessageIO
   end
 
 end   # module Net
