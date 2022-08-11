2f6477ad0a05236c38984d2a4d998fa26c23fb4b
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-08-11 21:07:14 +0900

    修正1

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
+  
   # GET /resource/password/new
+
   # def new
   #   super
   # end
--- vendor/bundle/ruby/2.7.0/gems/devise-4.8.1/app/controllers/devise/passwords_controller.rb
+++ vendor/bundle/ruby/2.7.0/gems/devise-4.8.1/app/controllers/devise/passwords_controller.rb
@@ -2,7 +2,7 @@
 
 class Devise::PasswordsController < DeviseController
   prepend_before_action :require_no_authentication
-  # Render the #edit only if coming from a reset password email link
+  #Render the edit only if coming from a reset password email link
   append_before_action :assert_reset_token_passed, only: :edit
 
   # GET /resource/password/new
