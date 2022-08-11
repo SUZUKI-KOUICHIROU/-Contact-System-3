d3753701adb0995e22937af6fdb90db1d9b2a4e8
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-08-11 12:45:24 +0900

    “パスワード変更3”

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,8 +61,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "samplehost"}
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: "38:f9:d3:72:a5:01" }
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
