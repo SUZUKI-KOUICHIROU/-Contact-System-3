e424d78cd5ee080f50a731d1b321fd1290ef9a93
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-08-11 11:16:04 +0900

    “パスワード変更2”

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,10 +59,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
   config.action_mailer.perform_caching = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: "samplehost"}
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
