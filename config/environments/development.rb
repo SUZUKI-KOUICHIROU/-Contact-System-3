cf93bbb54d27f6681c1841e382208567af298248
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-08-19 18:18:54 +0900

    “修正10”

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,19 +59,17 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.action_mailer.perform_caching = true
   config.action_mailer.raise_delivery_errors = true
-  
-  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.default_url_options = { host: 'https://arcane-peak-28945.herokuapp.com' }
-  
+
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+  host = 'https://arcane-peak-28945.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
     :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => host,
     :authentication => :plain,
   }
 
