e99ee8d4b636331f5ff56f22a7faf5b2d10b414f
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-08-19 16:36:07 +0900

    “修正6”

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,8 +62,8 @@ Rails.application.configure do
   config.action_mailer.perform_caching = true
   config.action_mailer.raise_delivery_errors = true
   
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  #config.action_mailer.default_url_options = { host: "arcane-peak-28945.herokuapp.com" }
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: "arcane-peak-28945.herokuapp.com" }
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
