355af0b8272ff9ebf46057e12450d8e4ce8e5eed
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-08-21 08:15:17 +0900

    “修正20”

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,16 +20,7 @@ class ApplicationController < ActionController::Base
 
   def set_student
     @student = Student.find(params[:id])
-  end
-  
-  # ログイン済みのユーザーか確認します。
-  #def logged_in_user
-    #unless logged_in?
-      #store_location
-      #flash[:danger] = "ログインしてください。"
-      #redirect_to login_url
-    #end
-  #end  
+  end 
   
   # アクセスしたユーザーが現在ログインしているユーザーか確認します。
   def correct_user
