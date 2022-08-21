087bd23c378ff56d1e06615c5709fe4e8a2d46ca
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-08-21 13:00:16 +0900

    “修正21”

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,19 @@ Rails.application.configure do
 
   host = 'samplehost'
   Rails.application.routes.default_url_options[:host] = host
+
+  config.action_mailer.perform_caching = true
+  config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://arcane-peak-28945.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => ENV["GOOGLE_MAIL_ADDRESS"],
+    :password => ENV["GOOGLE_MAILER_PASSWORD"],
+    :authentication => 'login'
+  }
 end
