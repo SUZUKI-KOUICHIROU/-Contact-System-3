0b9e2c58909483dad06793e01421dd808d63857a
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-10-06 14:19:33 +0900

    “修正10”

--- config/initializers/omniauth/strategies/line.rb
+++ config/initializers/omniauth/strategies/line.rb
@@ -1,18 +1,1 @@
-require 'omniauth-oauth2'
 
-module OmniAuth
-  module Strategies
-    class Line < OmniAuth::Strategies::OAuth2
-      option :scope, 'email profile openid' #追記
-
-      info do
-        {
-          name:        raw_info['displayName'],
-          image:       raw_info['pictureUrl'],
-          description: raw_info['statusMessage'],
-          email:    JWT.decode(access_token.params['id_token'], ENV['LINE_CHANNEL_SECRET']).first['email'] 
-        }
-      end
-    end
-  end
-end
