7cd26d6865b74deba17bbc1528adca9c043f1e8c
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-10-08 17:41:44 +0900

    “修正9”

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'https://arcane-peak-28945.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://evening-anchorage-99045.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
