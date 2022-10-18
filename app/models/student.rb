3f560a6f8a9d3a0b3121092a0c89b12d4682d46e
KOUICHIROUSUZUKI <51381437+KOUICHIROUSUZUKI@users.noreply.github.com>
2022-10-19 08:56:22 +0900

    “検索機能追加15”

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,8 @@ class Student < ApplicationRecord
   belongs_to :user
   
   validates :birthday, presence: true
-  validates :student_number, length: { maximum: 2 }
+  validates :student_number, allow_blank: true, numericality: {only_integer: true}, length: { maximum: 2 }
+
 
   def self.search(keyword)
     where(["student_name like?", "%#{keyword}%"])
